12a8bba353f1d8029c88213265e1ea69ec87dff7
worr13d <feltoncookies123@gmail.com>
2025-12-24 13:56:43 -0500

    initial commit 2

---
+++ Games/ASCII Dungeon/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASCII_Dungeon
+{
+    internal class Player
+    {
+        private int health;
+        private int money;
+        private Weapon[] weapons;
+        private Weapon weapon;
+        
+    }
+}
